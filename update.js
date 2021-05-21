c21b313d9e0e253d5f60e670253fe75ee3ca45f9
santiago diaz <sidiaz4@miuandes.cl>
2021-05-20 21:21:59 -0400

    final2.0

--- update.js
+++ update.js
@@ -2,13 +2,13 @@ function update(yN, number, t1, t2, model){
     if (t1 === 'Celsius' && t2 === 'Fahrenheit')
         CF = number*1.8 + 32
     else if (t1 === 'Celsius' && t2 === 'Kelvin')
-        CF = number + 273.15
+        CF = Number(number) + 273.15
     else if (t1 === 'Fahrenheit' && t2 === 'Celsius')
         CF = (number - 32)/1.8
     else if (t1 === 'Fahrenheit' && t2 === 'Kelvin')
         CF = (number + 459.67)*(5/9)
     else if (t1 === 'Kelvin' && t2 === 'Celsius')
-        CF = number - 273.15
+        CF = Number(number) - 273.15
     else if (t1 === 'Kelvin' && t2 === 'Fahrenheit')
         CF = number*(9/5) - 459.67
     else CF = number
